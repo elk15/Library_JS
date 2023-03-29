f0507de4a3453f0609f62881268100489f6d0f3a
elina <papadelina145@gmail.com>
2023-03-29 10:43:29 +0300

    Create object constructor for books and a function to add the books in an array.

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
