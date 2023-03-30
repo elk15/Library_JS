fa77ec8f74a860a5dbeb1bb9e057351ec31c3540
elina <papadelina145@gmail.com>
2023-03-30 15:16:50 +0300

    Add function to display books

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const myLibrary = [];
+const addBookBtn = document.querySelector('.add-book');
+const cardsDiv = document.querySelector('.cards');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -11,3 +13,25 @@ function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
+
+function displayBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const newDiv = document.createElement('div');
+        newDiv.appendChild(document.createElement('p'))
+            .appendChild(document.createTextNode(myLibrary[i].title));
+        newDiv.appendChild(document.createElement('p'))
+            .appendChild(document.createTextNode(myLibrary[i].author));
+        newDiv.appendChild(document.createElement('p'))
+            .appendChild(document.createTextNode(`${myLibrary[i].pages} pages`));
+        newDiv.appendChild(document.createElement('button'))
+            .appendChild(document.createTextNode(myLibrary[i].read));
+        newDiv.appendChild(document.createElement('button'))
+            .appendChild(document.createTextNode('Remove'));
+        newDiv.classList.add('card');
+        cardsDiv.appendChild(newDiv);
+    }
+}
+
+addBookToLibrary('"Pride and Prejustice"', 'Jane Austen', 430, false);
+addBookToLibrary('"The Hobbit"', 'J.R.R. Tolkien', 366, true);
+displayBooks();
