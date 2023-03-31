dcb5e675c1ea84adf8d9620bb5063000640347a4
elina <papadelina145@gmail.com>
2023-03-31 12:24:15 +0300

    Refactored code to make a new createCard function and finished implementing the add new book button

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const cardsDiv = document.querySelector('.cards');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
+// get form elements
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const isReadInput = document.querySelector('#is-read');
+const modalForm = document.querySelector('.modal-form');
+
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
@@ -11,45 +18,69 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead;
 }
 
+function createCard(book) {
+    const newDiv = document.createElement('div');
+    const titleP = document.createElement('p');
+    const authorP = document.createElement('p');
+    const pagesP = document.createElement('p');
+    const isReadBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+
+    titleP.textContent = book.title;
+    authorP.textContent = book.author;
+    pagesP.textContent = book.pages;
+    isReadBtn.textContent = book.isRead;
+    removeBtn.textContent = 'Remove';
+
+    newDiv.appendChild(titleP);
+    newDiv.appendChild(authorP);
+    newDiv.appendChild(pagesP);
+    newDiv.appendChild(isReadBtn);
+    newDiv.appendChild(removeBtn);
+
+    newDiv.classList.add('card');
+    removeBtn.classList.add('remove-btn');
+
+    cardsDiv.appendChild(newDiv);
+}
+
+// add create card function here
 function addBookToLibrary(title, author, pages, isRead) {
     const book = new Book(title, author, pages, isRead);
     myLibrary.push(book);
+    createCard(book);
 }
 
-function displayBooks() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        const newDiv = document.createElement('div');
-        const removeBtn = document.createElement('button');
-
-        removeBtn.classList.add('remove-btn');
-        removeBtn.appendChild(document.createTextNode('Remove'));
-
-        newDiv.appendChild(document.createElement('p'))
-            .appendChild(document.createTextNode(myLibrary[i].title));
-        newDiv.appendChild(document.createElement('p'))
-            .appendChild(document.createTextNode(myLibrary[i].author));
-        newDiv.appendChild(document.createElement('p'))
-            .appendChild(document.createTextNode(`${myLibrary[i].pages} pages`));
-        newDiv.appendChild(document.createElement('button'))
-            .appendChild(document.createTextNode(myLibrary[i].isRead));
-        newDiv.appendChild(removeBtn);
-
-        newDiv.classList.add('card');
-        cardsDiv.appendChild(newDiv);
-    }
-}
-
-addBookBtn.addEventListener('click', (event) => {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-    event.preventDefault();
-});
-
-overlay.addEventListener('click', () => {
+function closeModal() {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
+}
+
+// open modal
+addBookBtn.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+});
+
+// close modal by clicking outside of it
+overlay.addEventListener('click', () => {
+    closeModal();
+});
+
+// submit data from form
+modalForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    const isRead = isReadInput.checked;
+    addBookToLibrary(title, author, pages, isRead);
+    closeModal();
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    isRead.checked = false;
 });
 
 addBookToLibrary('"Pride and Prejustice"', 'Jane Austen', 430, false);
 addBookToLibrary('"The Hobbit"', 'J.R.R. Tolkien', 366, true);
-displayBooks();
